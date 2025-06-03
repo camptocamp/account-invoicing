e53be6305459827a2bc6900037f48908f7001160
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2025-06-03 17:27:02 +0200

    [IMP] account_invoice_section_sale_order: speed up
    On large invoices, at least on 17.0, rewriting the sequence of the
    invoice lines is very slow, as it triggers a flush to disk for each line
    (this is even worse when the database is lot running on the same
    server).
    
    This commit implements a different method for inserting the sections in
    the invoice lines: we precreate the lines with spaced out sequence
    numbers, and then we can add the sections between the existing lines,
    which is much faster.

---
+++ account_invoice_section_sale_order/models/sale_order_line.py
@@ -0,0 +1,21 @@
+# Copyright 2025 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _prepare_invoice_line(self, **optional_values):
+        values = super()._prepare_invoice_line(**optional_values)
+        current_sequence = self.env.context.get("current_line_sequence")
+        if current_sequence is None:
+            return values
+        # update the sequence of the invoice lines, add some space between lines so that
+        # we can insert the sections afterwards.
+        # we use a mutable in the context to be able to update it and have the updated
+        # values in the different calls to _prepare_invoice_line
+        values["sequence"] = current_sequence[0]
+        current_sequence[0] += 10
+        return values
